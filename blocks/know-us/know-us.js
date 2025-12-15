8804d625f1877575558b634ea92f836cd41bbaf2
AdityaA135 <adityaagarwal664@gmail.com>
2025-12-16 00:11:35 +0530

    eds

--- blocks/know-us/know-us.js
+++ blocks/know-us/know-us.js
@@ -38,7 +38,7 @@ export default async function decorate(block) {
     })();
 
     console.log("title: ",title);
-    console.log("sub heading: ",sub_heading;
+    console.log("sub heading: ",sub_heading);
     console.log("Desc: ",desc);
     // Clear old authored HTML
     block.innerHTML = "";
